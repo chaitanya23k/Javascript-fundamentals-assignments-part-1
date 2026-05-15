8b44bcf038b3ce361daccbc1c14b23377a623d47
chaitanya kolla <107527208+chaitanya23k@users.noreply.github.com>
2026-05-15 13:09:35 +0530

    Create datatype.js

---
+++ datatype.js
@@ -0,0 +1,26 @@
+//in js we can use camelcase in variables or $ sign or _ is valid but not numbers.
+
+// There are more data types in js..in js every value is an object or primitive value..
+//object will look like in curly braces...while primitive value look like normal.
+//we can learn objects later now just focus on primitive data types.
+//There are 7 data types number,boolean,string,undefined,null,symbol and bigint.
+//I think everyone already know what is number,string,boolean...so here undefined means it is an empty value which is not defined in variable, null means it is an
+//null value, symbol means a value can't be changed, bigint means larger integers than the numbers can hold.
+
+//Assignment
+//Declare a variable called isIsland and set its value according to your country. The variable should hold a Boolean value. 
+//Also declare a variable language, but don't assign it any value yet.
+//Log the types of isIsland, population, country and language to the console.
+
+let isIsland = "Maldives"
+let maldives = true
+console.log(maldives)
+console.log(isIsland)
+let language;
+let population = 532000
+let country = "Maldives"
+console.log(typeof language)
+console.log(population)
+console.log(typeof population)
+console.log(country)
+console.log(typeof country)
